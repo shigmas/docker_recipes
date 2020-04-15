7d01e31aac0bced62db28a14491ba16fe6219489
Masa <wyan@tinkermode.com>
2020-04-16 08:41:32 +0900

    some tweaks
    copy the scripts to the local directory. build.sh will fetch raspbian and mount it.

--- arm/scripts/build.sh
+++ arm/scripts/build.sh
@@ -2,4 +2,11 @@
 
 echo Running docker image
 uname -a
-/bin/ls
+ls /usr/bin/qemu*
+
+./get-rasbian.sh
+
+./mount-raspbian.sh ./2020-02-13-raspbian-buster-lite.img
+
+echo `ls /mnt/root`
+echo `ls /mnt/boot`
--- rpi/tools/get-rasbian.sh
+++ rpi/tools/get-rasbian.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-URLDIR=https://downloads.raspberrypi.org/raspbian_full/images/raspbian_full-2020-02-14
-VERSION_FILE=2020-02-13-raspbian-buster-full
+#URLDIR=https://downloads.raspberrypi.org/raspbian_full/images/raspbian_full-2020-02-14
+#VERSION_FILE=2020-02-13-raspbian-buster-full
+URLDIR=https://downloads.raspberrypi.org/raspbian_lite/images/raspbian_lite-2020-02-14
+VERSION_FILE=2020-02-13-raspbian-buster-lite
 ZIP_FILE=$VERSION_FILE.zip
 IMAGE_FILE=$VERSION_FILE.img
 
