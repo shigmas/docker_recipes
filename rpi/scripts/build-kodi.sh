45d67ec4f1c7cb7923d45ef814cae320fc88d696
Masa <wyan@tinkermode.com>
2020-04-20 09:48:30 +0900

    moved rpi tools to scripts to match arm, which makes more sense.

---
+++ rpi/scripts/build-kodi.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo Running build-kodi
+
+./resize-raspbian.sh $DATA_DIR/$RASPBIAN_VERSION.img 7678
+./mount-raspbian.sh $DATA_DIR/$RASPBIAN_VERSION-7678.img
+./run-raspbian.sh
+./unmount-raspbian.sh
--- rpi/tools/build-kodi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo Running build-kodi
-
-./resize-raspbian.sh $WORK_DIR/$RASPBIAN_VERSION.img 7678
-./mount-raspbian.sh $WORK_DIR/$RASPBIAN_VERSION-7678.img
-./run-raspbian.sh
-./unmount-raspbian.sh
