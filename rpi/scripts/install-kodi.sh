03923231cc00ae02c00a56f4f5fef844f61292ae
Masa <wyan@tinkermode.com>
2020-04-26 07:34:30 +0900

    yocto.sato build
    Building a sample yocto image in docker. It's just the test sato image, with no modifications, just to see how yocto works in Docker.
    
    Also, tried a kodi build tweak to set the locale.

--- rpi/scripts/install-kodi.sh
+++ rpi/scripts/install-kodi.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-update-locale LC_ALL="en_US.UTF-8"
+locale-gen en_US.UTF-8 && update-locale LC_ALL=en_US.UTF-8 \
+    LANG=en_US.UTF-8
+LANG en_US.UTF-8
+LC_ALL en_US.UTF-8
+
 apt-get update
 apt-get install -y kodi 
